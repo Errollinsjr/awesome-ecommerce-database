82197d755d4cf2d2dbbd88ad523319fc48883d00
Errollinsjr <errollins.jr@gmail.com>
2021-07-21 23:48:27 -0400

    added get routes for category

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -5,11 +5,32 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
+  try {
+    const categoryData = await Category.findAll({
+      include: [{ model: Product }]
+    });
+    res.status(200).json(categoryData);
+  } 
+  catch (err) {
+    res.status(400).json(err)
+  }
   // be sure to include its associated Products
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }]
+    });
+    if(!categorytData) {
+      res.status(400).json({'message': "Category id is not found, please utilize valid id"});
+    }
+    res.status(200).json(categoryData);
+  } 
+  catch (err) {
+    res.status(400).json(err)
+  }
   // be sure to include its associated Products
 });
 
